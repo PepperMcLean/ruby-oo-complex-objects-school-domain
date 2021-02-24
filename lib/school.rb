f2611b0e10c5e3515e25d6ee74acd10783c22bbf
PepperMcLean <jackmcl206@gmail.com>
2021-02-24 11:50:51 -0600

    commit tree 5336b676ddba71c1a39a61185de14a35efdcc2fe

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    attr_accessor :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if not @roster.include?(grade)
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        return @roster.fetch(grade)
+    end
+
+    def sort
+        @roster.each do |grade, name|
+            @roster[grade] = name.sort
+        end
+        @roster
+    end
+
+
+end
